b45012619dd40791387593aa4d930bdb9bc965d1
galtnie <hereby@protonmail.com>
2019-06-13 14:47:26 +0300

    Updates

---
+++ precache-manifest.2f34d81df2e0dfb5ad434c57411b0245.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f4239a83b03bfd42ba124c3d0580462",
+    "url": "/booking_test_redux/index.html"
+  },
+  {
+    "revision": "c069e49c07c53f6b7e3a",
+    "url": "/booking_test_redux/static/css/main.9150a668.chunk.css"
+  },
+  {
+    "revision": "02844479796a9828f580",
+    "url": "/booking_test_redux/static/js/2.574dcb91.chunk.js"
+  },
+  {
+    "revision": "c069e49c07c53f6b7e3a",
+    "url": "/booking_test_redux/static/js/main.8c1ea7df.chunk.js"
+  },
+  {
+    "revision": "95a4e4397fcc79036960",
+    "url": "/booking_test_redux/static/js/runtime~main.269719a0.js"
+  }
+]);
--- src/styles/CircularProgressMUIstyles.js
+++
@@ -1,7 +0,0 @@
-const styles = theme => ({
-    progress: {
-      margin: theme.spacing(2),
-    },
-  });
-
-  export default styles
