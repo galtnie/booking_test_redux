71400fe0cbfa7da226589ca558e691f55451072d
galtnie <hereby@protonmail.com>
2019-06-14 11:14:16 +0300

    fixed schedule

--- src/components/RoomDeterminer.js
+++ src/components/RoomDeterminer.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { checkIfSlotReserved, checkIfSlotSelected, } from '../functions'
-import history from '../history'
 import { selectSlot, unselectSlot, determineReservedSlots } from '../actions'
 import { FreeHome, Free, Busy, Chosen } from './Rooms.js'
 
 class RoomStatusDeterminer extends React.Component {
     render() {
         const reservedSlot = checkIfSlotReserved(this.props.reservedSlots, this.props.id)
-        if (history.location.pathname !== '/booking') {
+        
+        if (this.props.user === null) {
             return reservedSlot ? <Busy id={reservedSlot} /> : <FreeHome id={this.props.id} />;
         } else {
             const selectedSlot = checkIfSlotSelected(this.props.selectedSlots, this.props.id)
@@ -30,12 +30,13 @@ class RoomStatusDeterminer extends React.Component {
 
 const mapStateToProps = (state, ownProps) => {
     const { id } = ownProps
-    let reservedSlots = state.reservedSlots
-    let selectedSlots = state.selectedSlots
-    let dateInput = state.dateInput
-    let tickets = state.tickets
-    let halls = state.halls
-    return { id, reservedSlots, selectedSlots, dateInput, tickets, halls };
+    const reservedSlots = state.reservedSlots
+    const selectedSlots = state.selectedSlots
+    const dateInput = state.dateInput
+    const tickets = state.tickets
+    const halls = state.halls
+    const user = state.user
+    return { id, reservedSlots, selectedSlots, dateInput, tickets, halls, user };
 };
 export default connect(mapStateToProps, {
     selectSlot,
